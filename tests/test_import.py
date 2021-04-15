a60fc21a6f5900a528560f7762f3ac93487b3aee
Philip Rehberger <me@philiprehberger.com>
2021-04-15 16:09:54 -0500

    Release v0.1.5

---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+"""Basic import test."""
+
+
+def test_import():
+    """Verify the package can be imported."""
+    import philiprehberger_str_case
+    assert hasattr(philiprehberger_str_case, "__name__") or True
